0a79bb0b4ba8cd4db7169ebf965e15929c7a6baa
Wang <zhiwen.wang@epitech.eu>
2021-03-25 13:52:08 +0100

    [MOD](Game.php) : edit run function and add checkWinner function

--- src/Game.php
+++ src/Game.php
@@ -1,16 +1,42 @@
 <?php
+require "Player.php";
+require "ReadInput.php";
+require "GameInformations.php";
 
 class Game
 {
-    /**
-     * @param $input 2 lignes, comprenant les infos de chaque joueur
-     * exemple: "Bod 100 7 5\nAlice 80 9 3"
-     */
-    public function __construct($input)
+    private $playerA;
+    private $playerB;
+    private $gameInformations;
+
+    public function __construct()
     {
+        $readInput = new ReadInput();
+        $this->playerA = new Player($readInput->getPlayerInfo());
+        $this->playerB = new Player($readInput->getPlayerInfo());
+        $this->gameInformations = new GameInformations();
     }
 
     public function run()
     {
+        while ($this->playerA->isAlive() && $this->playerB->isAlive()) {
+            $this->playerB->applyDamage($this->playerA->getAttack(), $this->playerA->getLifePoints());
+            $this->playerA->applyDamage($this->playerB->getAttack(), $this->playerB->getLifePoints());
+            $this->gameInformations->displayGameInformations($this->playerA->GetLifePoints(), $this->playerB->GetLifePoints());
+        }
+        $this->checkWinner();
+    }
+
+    private function checkWinner()
+    {
+        if ($this->playerA->isAlive())
+        {
+            $this->gameInformations->displayGameResult($this->playerA->getName(), $this->playerA->getLifePoints());
+        }
+        else if ($this->playerB->isAlive())
+        {
+            $this->gameInformations->displayGameResult($this->playerB->getName(), $this->playerB->getLifePoints());
+        }
     }
 }
+?>
